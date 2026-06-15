1c307e61f92c836a9f8bb6e473e3d01121354078
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-06-16 00:07:05 +0200

    change layout to gap-3

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -6,12 +6,14 @@ export default function MainLayout({ children }) {
     return (
         <div className="min-h-screen flex flex-col p-4">
             <Header />
-            <main className="flex flex-1 gap-8 flex-col md:flex-row">
+            <main className="flex flex-1 gap-3 flex-col md:flex-row">
                 <aside className="order-2 md:order-1 min-w-0 lg:w-110">
                     <Sidebar title="Navigation" />
                 </aside>
 
-                <section className="order-1 md:order-2 flex-1 min-w-0 flex flex-col">{children}</section>
+                <section className="order-1 md:order-2 flex-1 min-w-0 flex flex-col">
+                    {children}
+                </section>
             </main>
             <Footer />
         </div>
