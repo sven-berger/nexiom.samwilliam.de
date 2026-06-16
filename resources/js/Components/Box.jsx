cce74bc1bc65e018a958e9d675170a48dd77bd58
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-06-16 13:29:06 +0200

    add more options to box component

--- resources/js/Components/Box.jsx
+++ resources/js/Components/Box.jsx
@@ -1,9 +1,9 @@
-export default function Box({ title, children, className = '' }) {
+export default function Box({ title, children, className = '', grow = false, noPadding = false, noFlex = false }) {
     if (!title) {
         return (
-            <div className="rounded-2xl">
+            <div className={`${grow ? 'grow ' : ''}rounded-2xl mb-3`}>
                 <div
-                    className={`space-y-2 h-full my-3 p-4 text-slate-700 border-t border-2 border-slate-300 bg-white rounded-2xl ${className}`}
+                    className={`text-slate-700 border-2 border-slate-300 bg-white rounded-2xl ${noPadding ? `${noFlex ? '' : 'flex flex-col '}overflow-hidden` : 'h-full my-3 p-4 space-y-2'} ${className}`}
                 >
                     {children}
                 </div>
@@ -11,12 +11,12 @@ export default function Box({ title, children, className = '' }) {
         );
     } else {
         return (
-            <div className="rounded-2xl">
+            <div className={`${grow ? 'grow ' : ''}flex flex-col rounded-2xl`}>
                 <h3 className="mt-3 text-lg border-b border-slate-300 bg-slate-200 p-4 font-semibold text-slate-700 rounded-t-2xl">
                     {title}
                 </h3>
                 <div
-                    className={`p-5 space-y-2 text-slate-700 border-t border border-slate-300 bg-white rounded-b-2xl ${className}`}
+                    className={`flex-1 text-slate-700 border-t border border-slate-300 bg-white rounded-b-2xl overflow-hidden ${noPadding ? '' : 'p-5 space-y-2'} ${className}`}
                 >
                     {children}
                 </div>
