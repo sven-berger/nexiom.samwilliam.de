5fd210f259349231b3b5bcebf9011a56b218a44d
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-06-16 23:53:46 +0200

    optmize

--- resources/js/Components/cv/Interests.jsx
+++ resources/js/Components/cv/Interests.jsx
@@ -6,26 +6,34 @@ export default function Interests() {
     return (
         <Box className="mb-5" title="Interessen">
             <div className="flex-1">
-                <ul class="auflistung">
-                    <li class="mb-3">
-                        <strong>Kontinuierlicher Ausbau meiner Website</strong>
+                <ul className="list-disc list-inside text-sm text-slate-700">
+                    <li className="mb-3">
+                        <span className="font-bold">Kontinuierlicher Ausbau meiner Website</span>
                         <br />
-                        <span>Redaktionell &amp; Technisch</span>
+                        <span className="text-gray-500 text-sm italic">
+                            Redaktionell &amp; Technisch
+                        </span>
                     </li>
-                    <li class="mb-3">
-                        <strong>Filme und Serien</strong>
+                    <li className="mb-3">
+                        <span className="font-bold">Filme und Serien</span>
                         <br />
-                        <span>bevorzugt realitätsnah und logisch aufgebaut</span>
+                        <span className="text-gray-500 text-sm italic">
+                            bevorzugt realitätsnah und logisch aufgebaut
+                        </span>
                     </li>
-                    <li class="mb-3">
-                        <strong>PlayStation</strong>
+                    <li className="mb-3">
+                        <span className="font-bold">PlayStation</span>
                         <br />
-                        <span>Vor allem storybasierte Rollenspiele (z. B. Assassin's Creed)</span>
+                        <span className="text-gray-500 text-sm italic">
+                            Vor allem storybasierte Rollenspiele (z. B. Assassin's Creed)
+                        </span>
                     </li>
                     <li>
-                        <strong>Regelmäßige Weiterbildung im IT-Bereich</strong>
+                        <span className="font-bold">Regelmäßige Weiterbildung im IT-Bereich</span>
                         <br />
-                        <span>Durch Tutorials, die dann in eigene Projekte einfließen</span>
+                        <span className="text-gray-500 text-sm italic">
+                            Durch Tutorials, die dann in eigene Projekte einfließen
+                        </span>
                     </li>
                 </ul>
             </div>
